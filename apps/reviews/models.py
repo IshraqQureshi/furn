e36c47261581289154463d182b9ba6d9e0813ce3
IshraqQureshi <56018643+IshraqQureshi@users.noreply.github.com>
2022-09-22 17:25:42 +0500

    Update models.py

--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -1,4 +1,3 @@
-from tkinter import CASCADE
 from django.db import models
 from apps.products.models import Product
 
@@ -6,7 +5,7 @@ from apps.products.models import Product
 class Review(models.Model):
     author = models.CharField(max_length=256)
     review = models.TextField()
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product)
 
     def __str__(self):
         return self.author
